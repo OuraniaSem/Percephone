3d131f975e001d83ecdc8a7bb5bcbbc724c46c84
cvandromme <celien.vandromme.etu@univ-lille.fr>
2024-07-03 17:49:22 +0200

    Multi session test and csv export beginning

--- percephone/cebra/cebra_decoding.py
+++ percephone/cebra/cebra_decoding.py
@@ -5,7 +5,7 @@ from numpy.random import uniform, randint
 from sklearn.model_selection import train_test_split
 
 from percephone.analysis.utils import get_zscore
-from percephone.utils.io import get_recs_dict
+from percephone.cebra.CEBRA_trials_viz import get_recs_dict
 
 recs = get_recs_dict("Célien")
 rec = recs[4456]
@@ -16,14 +16,14 @@ cebra_model = cebra.CEBRA(
     batch_size=512,
     learning_rate=1e-4,
     max_iterations=10000,
-    max_adapt_iterations=200, # TODO(user): to change to ~100-500
+    max_adapt_iterations=200, #to change to ~100-500
     time_offsets=10,
     output_dimension=8,
     verbose=False
 )
 
 # 2. Load example data
-neural_data = get_zscore(rec, exc_neurons=True, inh_neurons=False, time_span="stim", window=0.5, estimator="Mean", sort=False, amp_sort=False)
+neural_data = get_zscore(rec, exc_neurons=True, inh_neurons=False, time_span="stim", window=0.5, estimator="Mean", sort=False, amp_sort=False, single_frame_estimator=True)
 # new_neural_data = cebra.load_data(file="neural_data.npz", key="new_neural")
 # continuous_label = cebra.load_data(file="auxiliary_behavior_data.h5", key="auxiliary_variables", columns=["continuous1", "continuous2", "continuous3"])
 discrete_label = rec.detected_stim
@@ -57,11 +57,11 @@ cebra_model.fit(train_data, train_discrete_label,)
 # cebra_model.fit(train_data, train_discrete_label, train_continuous_label)
 
 # 5. Save the model
-tmp_file = Path(tempfile.gettempdir(), 'cebra.pt')
-cebra_model.save(tmp_file)
+# tmp_file = Path(tempfile.gettempdir(), 'cebra.pt')
+# cebra_model.save(tmp_file)
 
 # 6. Load the model and compute an embedding
-cebra_model = cebra.CEBRA.load(tmp_file)
+# cebra_model = cebra.CEBRA.load(tmp_file)
 train_embedding = cebra_model.transform(train_data)
 valid_embedding = cebra_model.transform(valid_data)
 # assert train_embedding.shape == (70, 8)
